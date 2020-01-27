8fa7119a8bf82c600647d0f99fda734f3bdd889c
mrdotx <klassiker@gmx.de>
2020-01-27 12:06:35 +0100

    typos corrected

--- polybar_rss.sh
+++ polybar_rss.sh
@@ -3,16 +3,16 @@
 # path:       ~/projects/polybar/polybar_rss.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/polybar
-# date:       2020-01-23T11:39:50+0100
+# date:       2020-01-27T11:58:31+0100
 
 # exit if newsboat is running
-pgrep -x newsboat > /dev/null 2>&1 \
-    && polybar-msg hook module/rss 2 > /dev/null 2>&1 \
+pgrep -x newsboat >/dev/null 2>&1 \
+    && polybar-msg hook module/rss 2 >/dev/null 2>&1 \
     && exit
 
-if ping -c1 -W1 -q 1.1.1.1 > /dev/null 2>&1; then
-    newsboat -x reload && newsboat -q -X > /dev/null 2>&1 \
-        && polybar-msg hook module/rss 3 > /dev/null 2>&1
+if ping -c1 -W1 -q 1.1.1.1 >/dev/null 2>&1; then
+    newsboat -x reload && newsboat -q -X >/dev/null 2>&1 \
+        && polybar-msg hook module/rss 3 >/dev/null 2>&1
 else
-    polybar-msg hook module/rss 3 > /dev/null 2>&1
+    polybar-msg hook module/rss 3 >/dev/null 2>&1
 fi
