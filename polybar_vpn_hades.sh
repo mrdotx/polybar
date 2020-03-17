e7e0a8cc4b5d982b565bc331bd882a1d66d1d04e
mrdotx <klassiker@gmx.de>
2020-03-17 04:32:02 +0100

    switched from nmcli to vpnc

--- polybar_vpn_hades.sh
+++ polybar_vpn_hades.sh
@@ -3,7 +3,7 @@
 # path:       ~/repos/polybar/polybar_vpn_hades.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/polybar
-# date:       2020-03-16T12:36:53+0100
+# date:       2020-03-17T04:23:37+0100
 
 vpn_name=hades
 grey="%{o$(xrdb -query | grep Polybar.foreground1: | cut -f2)}%{o-}"
@@ -11,7 +11,7 @@ red="%{o$(xrdb -query | grep color9: | cut -f2)}%{o-}"
 
 case "$1" in
     --status)
-        if [ "$(nmcli connection show --active $vpn_name)" ]
+        if [ "$(pgrep -f "vpnc $vpn_name")" ]
         then
             printf "%s" "$red"
         else
@@ -19,14 +19,12 @@ case "$1" in
         fi
         ;;
     *)
-        if [ "$(nmcli connection show --active $vpn_name)" ]
+        if [ "$(pgrep -f "vpnc $vpn_name")" ]
         then
-            nmcli con down id $vpn_name \
+            sudo vpnc-disconnect \
                 && printf "%s" "$grey"
         else
-            gpg -o "/tmp/$vpn_name.gpg.txt" "$HOME/cloud/webde/Keys/$vpn_name.gpg" \
-                && nmcli con up id $vpn_name passwd-file "/tmp/$vpn_name.gpg.txt" \
-                && rm -f "/tmp/$vpn_name.gpg.txt" \
+            sudo vpnc $vpn_name \
                 && printf "%s" "$red"
         fi
         ;;
