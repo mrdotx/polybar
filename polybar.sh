d4c2183f2277284d134db19559d5c6e6d02add52
mrdotx <klassiker@gmx.de>
2020-03-29 16:54:52 +0200

    small fixes

--- polybar.sh
+++ polybar.sh
@@ -3,9 +3,12 @@
 # path:       ~/.local/share/repos/polybar/polybar.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/polybar
-# date:       2020-03-29T13:48:12+0200
+# date:       2020-03-29T16:45:59+0200
 
 dual_bar=1
+
+pri=$(polybar -m | grep "(primary)" | sed -e 's/:.*$//g')
+sec=$(polybar -m | grep -v "(primary)" | sed -e 's/:.*$//g')
 
 # terminate already running bar instances
 killall -q polybar
@@ -14,9 +17,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 0.1; done
 
 # launch polybar
-pri=$(polybar -m | grep "(primary)" | sed -e 's/:.*$//g')
 if [ "$dual_bar" = 1 ] && [ "$(polybar -m | wc -l)" = 2 ]; then
-    sec=$(polybar -m | grep -v "(primary)" | sed -e 's/:.*$//g')
     MONITOR=$pri polybar i3_2_mon_pri &
     MONITOR=$sec polybar i3_2_mon_sec &
 else
