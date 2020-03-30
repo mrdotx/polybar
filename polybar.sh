58845c3322e1cfa4ee1b8460cd50cc3e54204bad
mrdotx <klassiker@gmx.de>
2020-03-30 08:59:48 +0200

    monitor selection optimized

--- polybar.sh
+++ polybar.sh
@@ -3,12 +3,9 @@
 # path:       ~/.local/share/repos/polybar/polybar.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/polybar
-# date:       2020-03-29T16:45:59+0200
+# date:       2020-03-30T08:55:41+0200
 
 dual_bar=1
-
-pri=$(polybar -m | grep "(primary)" | sed -e 's/:.*$//g')
-sec=$(polybar -m | grep -v "(primary)" | sed -e 's/:.*$//g')
 
 # terminate already running bar instances
 killall -q polybar
@@ -17,7 +14,9 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 0.1; done
 
 # launch polybar
+pri=$(polybar -m | grep "(primary)" | sed -e 's/:.*$//g')
 if [ "$dual_bar" = 1 ] && [ "$(polybar -m | wc -l)" = 2 ]; then
+    sec=$(polybar -m | grep -v "(primary)" | sed -e 's/:.*$//g')
     MONITOR=$pri polybar i3_2_mon_pri &
     MONITOR=$sec polybar i3_2_mon_sec &
 else
