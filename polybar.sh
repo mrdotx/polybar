2a272c10eba9b34dc2e028cc1e53f03066efde19
mrdotx <klassiker@gmx.de>
2020-05-21 21:35:14 +0200

    toggle for dual-/single bar added

--- polybar.sh
+++ polybar.sh
@@ -3,25 +3,65 @@
 # path:       /home/klassiker/.local/share/repos/polybar/polybar.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/polybar
-# date:       2020-05-20T15:18:28+0200
+# date:       2020-05-21T21:24:50+0200
+
+script=$(basename "$0")
+help="$script [-h/--help] -- script to start polybar
+  Usage:
+    $script [settings]
+
+  Settings:
+    without given setting re-/start polybar
+    -t = toggle between dual and single bar
+
+  Example:
+    $script
+    $script -t"
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    printf "%s\n" "$help"
+    exit 0
+fi
 
 # xresources
 dual_bar=$(printf "%s" "$(xrdb -query | grep Polybar.dualbar: | cut -f2)")
 
-# terminate already running bar instances
-killall -q polybar
+tog(){
+    file="$HOME/.config/xorg/Xresources"
+    if [ "$dual_bar" = true ]; then
+        sed -i "/Polybar.dualbar:/c\Polybar.dualbar:                    false" "$file"
+    else
+        sed -i "/Polybar.dualbar:/c\Polybar.dualbar:                    true" "$file"
+    fi
+    xrdb -merge "$file" \
+        && systemctl --user restart polybar.service
+}
 
-# wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 0.1; done
+st(){
+    # terminate already running bar instances
+    killall -q polybar
 
-# launch polybar
-pri=$(polybar -m | grep "(primary)" | sed -e 's/:.*$//g')
-sec=$(polybar -m | grep -v "(primary)" | sed q1 | sed -e 's/:.*$//g')
+    # wait until the processes have been shut down
+    while pgrep -x polybar >/dev/null; do sleep 0.1; done
 
-if [ "$dual_bar" = true ] && [ "$(polybar -m | wc -l)" -ge 2 ]; then
-    MONITOR=$pri polybar i3_2_mon_pri &
-    MONITOR=$sec polybar i3_2_mon_sec &
-else
-    [ -n "$pri" ] && sec="$pri"
-    MONITOR=$sec polybar i3_1_mon &
-fi
+    # launch polybar
+    pri=$(polybar -m | grep "(primary)" | sed -e 's/:.*$//g')
+    sec=$(polybar -m | grep -v "(primary)" | sed q1 | sed -e 's/:.*$//g')
+
+    if [ "$dual_bar" = true ] && [ "$(polybar -m | wc -l)" -ge 2 ]; then
+        MONITOR=$pri polybar i3_2_mon_pri &
+        MONITOR=$sec polybar i3_2_mon_sec &
+    else
+        [ -n "$pri" ] && sec="$pri"
+        MONITOR=$sec polybar i3_1_mon &
+    fi
+}
+
+case "$1" in
+    -t)
+        tog
+        ;;
+    *)
+        st
+        ;;
+esac
