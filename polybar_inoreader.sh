3fe11af8c8a583b0169ed8d2f27ffd88922d07dd
mrdotx <klassiker@gmx.de>
2022-04-04 08:36:20 +0200

    minor optimizations

--- polybar_inoreader.sh
+++ polybar_inoreader.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/polybar/polybar_inoreader.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/polybar
-# date:   2022-04-03T11:15:58+0200
+# date:   2022-04-04T08:33:29+0200
 
 icon_rss=""
 icon_star=""
@@ -20,6 +20,21 @@ pass=$( \
         | head -n1 \
 )
 
+output() {
+    # get xresources
+    xrdb_query() {
+        xrdb -query \
+            | grep "$1:" \
+            | cut -f2
+    }
+
+    printf "%%{o%s}%%{F%s}%s %s%%{F- o-}\n" \
+        "$(xrdb_query "$1")" \
+        "$(xrdb_query "$2")" \
+        "$3" \
+        "$4"
+}
+
 extract_data() {
     printf "%s" "$request" \
         | awk -F "$1" '{print $2}' \
@@ -28,33 +43,24 @@ extract_data() {
         | tr -d "\""
 }
 
-xrdb_query() {
-    xrdb -query \
-        | grep "$1:" \
-        | cut -f2
-}
-
-if sleep 3 && ping -c1 -W1 -q 1.1.1.1 >/dev/null 2>&1; then
-    auth=$( \
-        curl --silent -s "$url_login?Email=$user&Passwd=$pass" \
-            | grep 'Auth=' \
-            | sed 's/Auth/auth/' \
+if sleep 1 && ping -c1 -W1 -q 1.1.1.1 >/dev/null 2>&1; then
+    auth=$(curl --silent -s "$url_login?Email=$user&Passwd=$pass" \
+        | grep 'Auth=' \
+        | sed 's/Auth/auth/' \
     )
 
-    request=$( \
-        curl --silent -H "Authorization: GoogleLogin $auth" \
-            "$url_request?AppId=$app_id&AppKey=$app_key" \
+    request=$(curl --silent -H "Authorization: GoogleLogin $auth" \
+        "$url_request?AppId=$app_id&AppKey=$app_key" \
     )
 
-    unreaded="$icon_rss $(extract_data 'reading-list",')"
-    starred="$icon_star $(extract_data 'starred",')"
+    output \
+        "$line_color" \
+        "$foreground_color" \
+        "$icon_rss $(extract_data 'reading-list",')" \
+        "$icon_star $(extract_data 'starred",')"
 else
-    unreaded="$icon_rss"
-    starred="$icon_star"
+    output \
+        "$line_color" \
+        "$foreground_color" \
+        "$icon_rss"
 fi
-
-printf "%%{o%s}%%{F%s}%s %s%%{F- o-}\n" \
-    "$(xrdb_query "$line_color")" \
-    "$(xrdb_query "$foreground_color")" \
-    "$unreaded" \
-    "$starred"
