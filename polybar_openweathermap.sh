393c0407d4042f46fe155efa7de82c9874c71954
mrdotx <klassiker@gmx.de>
2022-04-11 20:42:50 +0200

    minor fixes and structural changes

--- polybar_openweathermap.sh
+++ polybar_openweathermap.sh
@@ -3,38 +3,19 @@
 # path:   /home/klassiker/.local/share/repos/polybar/polybar_openweathermap.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/polybar
-# date:   2022-04-11T16:15:36+0200
-
-city=""
-gpg_file="$HOME/.local/share/repos/password-store/www/development/openweathermap.gpg"
-
-get_icon() {
-    case  $1 in
-        # https://openweathermap.org/weather-conditions
-        01d) icon="";;
-        01n) icon="";;
-        02d) icon="";;
-        02n) icon="";;
-        03*) icon="";;
-        04*) icon="";;
-        09d) icon="";;
-        09n) icon="";;
-        10*) icon="";;
-        11*) icon="";;
-        13*) icon="";;
-        50*) icon="";;
-        *)   icon="";;
-    esac
-
-    printf "%s" "$icon"
-}
+# date:   2022-04-11T20:33:26+0200
 
 request() {
+    city=
+    gpg_file="$HOME/.local/share/repos/password-store/www/development/openweathermap.gpg"
+
     get_location() {
         url_geo="https://location.services.mozilla.com/v1/geolocate?key=geoclue"
 
-        if [ -n "$city" ]; then
-            printf "%s" "q=$city"
+        if [ "$1" -gt 0 ]; then
+            printf "%s" "id=$1"
+        elif [ -n "$1" ]; then
+            printf "%s" "q=$*"
         else
             data=$(curl -sf "$url_geo")
 
@@ -54,30 +35,39 @@ request() {
     }
 
     get_apikey() {
-        gpg -q -d "$gpg_file" \
+        gpg -q -d "$1" \
             | grep "^api_key =" \
             | awk -F ' = ' '{print $2}'
     }
 
     url_api="https://api.openweathermap.org/data/2.5"
-    url_para="appid=$(get_apikey)&mode=xml&units=metric"
+    url_para="appid=$(get_apikey "$gpg_file")&mode=xml&units=metric"
 
-    curl -sf "$url_api/$1?$url_para&$(get_location)"
-}
-
-extract_data() {
-    tag="$1"
-    value="$2"
-    shift 2
-
-    printf "%s\n" "$@" \
-        | awk -F "<$tag" '{print $2}' \
-        | awk -F "$value=" '{print $2}' \
-        | cut -d'"' -f2 \
-        | sed '/^$/d'
+    curl -sf "$url_api/$1?$url_para&$(get_location "$city")"
 }
 
 get_data() {
+    get_icon() {
+        case  $1 in
+            # https://openweathermap.org/weather-conditions
+            01d) icon="";;
+            01n) icon="";;
+            02d) icon="";;
+            02n) icon="";;
+            03*) icon="";;
+            04*) icon="";;
+            09d) icon="";;
+            09n) icon="";;
+            10*) icon="";;
+            11*) icon="";;
+            13*) icon="";;
+            50*) icon="";;
+            *)   icon="";;
+        esac
+
+        printf "%s" "$icon"
+    }
+
     convert_date() {
         case $2 in
             Epoch)
@@ -87,6 +77,18 @@ get_data() {
                 date -d "@$1" +%H:%M
                 ;;
         esac
+    }
+
+    extract_data() {
+        tag="$1"
+        value="$2"
+        shift 2
+
+        printf "%s\n" "$@" \
+            | awk -F "<$tag" '{print $2}' \
+            | awk -F "$value=" '{print $2}' \
+            | cut -d'"' -f2 \
+            | sed '/^$/d'
     }
 
     current=$(request "weather")
@@ -115,9 +117,10 @@ get_data() {
     sunrise=$(convert_date "$current_sunrise" "Epoch")
     sunset=$(convert_date "$current_sunset" "Epoch")
 
-    if [ "$sunrise" -ge "$now" ]; then
+    if [ "$sunrise" -ge "$now" ] \
+        || [ "$now" -gt "$sunset" ]; then
         daytime=" $(convert_date "$sunrise")"
-    elif [ "$sunset" -gt "$now" ]; then
+    elif [ "$sunset" -ge "$now" ]; then
         daytime=" $(convert_date "$sunset")"
     fi
 }
