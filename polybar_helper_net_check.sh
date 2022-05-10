52e90cd9e394e1944b954596d203bbea55649ff8
mrdotx <klassiker@gmx.de>
2022-05-10 15:41:57 +0200

    useless check removed

--- polybar_helper_net_check.sh
+++ polybar_helper_net_check.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/polybar/polybar_helper_net_check.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/polybar
-# date:   2022-04-10T17:57:16+0200
+# date:   2022-05-10T09:56:12+0200
 
 # check ip/address to connect to (default 1.1.1.1)
 net=${1:-1.1.1.1}
@@ -15,9 +15,3 @@ while ! ping -c1 -W1 -q "$net" >/dev/null 2>&1 \
         sleep .1
         check=$((check - 1))
 done
-
-if [ $check -eq 0 ]; then
-    return 1
-else
-    return 0
-fi
