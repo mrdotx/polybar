d55665563f7ae1da982e009fd99b4283cb996482
mrdotx <klassiker@gmx.de>
2022-06-12 23:03:25 +0200

    minor icon format changes

--- polybar_services.sh
+++ polybar_services.sh
@@ -3,28 +3,28 @@
 # path:   /home/klassiker/.local/share/repos/polybar/polybar_services.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/polybar
-# date:   2022-06-12T19:11:40+0200
+# date:   2022-06-12T22:57:37+0200
 
 # speed up script by using standard c
 LC_ALL=C
 LANG=C
 
-icon_autolock="ﱱ"
-icon_autotiling="侀"
-icon_compositor="頋"
-icon_mousepointer=""
-icon_resolver=""
-icon_timesync="祥"
-icon_ssh="撚"
-icon_vpn="旅"
-icon_printer="朗"
-icon_bluetooth=""
+icon_autolock="%{T2}ﱱ %{T-} "
+icon_autotiling="%{T2}侀 %{T-}"
+icon_compositor="%{T2}頋 %{T-}"
+icon_mousepointer="%{T2}%{T-} "
+icon_resolver="%{T2} %{T-}"
+icon_timesync="%{T2}祥 %{T-}"
+icon_ssh="%{T2}撚 %{T-}"
+icon_vpn="%{T2}旅 %{T-}"
+icon_printer="%{T2}朗 %{T-}"
+icon_bluetooth="%{T2}%{T-} "
 
 set_output() {
     if [ -z "$services" ]; then
         services="$(printf "%s" "$1")"
     else
-        services="$(printf "%s %s" "$services" "$1")"
+        services="$(printf "%s%s" "$services" "$1")"
     fi
 }
 
@@ -54,7 +54,7 @@ case "$1" in
         service_status "cups.service" "$icon_printer"
         service_status "bluetooth.service" "$icon_bluetooth"
 
-        polybar_helper_output.sh "%{T2}$services%{T-}"
+        polybar_helper_output.sh "$services"
         ;;
     --update)
         for id in $(pgrep -f "polybar main"); do
