d20a005b77d8f162705fc2cc8065fad47a38e682
mrdotx <klassiker@gmx.de>
2022-11-28 11:11:26 +0100

    starred count added

--- polybar_freshrss.sh
+++ polybar_freshrss.sh
@@ -3,18 +3,20 @@
 # path:   /home/klassiker/.local/share/repos/polybar/polybar_freshrss.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/polybar
-# date:   2022-11-26T15:06:24+0100
+# date:   2022-11-28T11:05:38+0100
 
 # speed up script by using standard c
 LC_ALL=C
 LANG=C
 
-request() {
-    url_login="http://pi/freshrss/api/greader.php/accounts/ClientLogin"
-    url_request="http://pi/freshrss/api/greader.php/reader/api/0/unread-count?output=json"
-    user="klassiker"
-    gpg_file="$HOME/.local/share/repos/password-store/www/development/freshrss.gpg"
+# config
+user="klassiker"
+gpg_file="$HOME/.local/share/repos/password-store/www/development/freshrss.gpg"
+url_login="http://pi/freshrss/api/greader.php/accounts/ClientLogin"
+url_request="http://pi/freshrss/api/greader.php/reader/api/0"
+url_parameter="?output=json"
 
+request() {
     get_pass() {
         gpg -q -d "$gpg_file" \
             | head -n1
@@ -26,15 +28,22 @@ request() {
             | sed 's/Auth/auth/'
     }
 
-    curl -fsS -H "Authorization:GoogleLogin $(get_auth)" "$url_request"
+    curl -fsS -H "Authorization:GoogleLogin $(get_auth)" \
+        "$url_request/$1$url_parameter"
 }
 
-extract_data() {
+get_count() {
     printf "%s" "$1" \
         | awk -F "$2" '{print $2}' \
         | cut -d ',' -f1 \
         | sed 's/"count"://' \
         | tr -d "\""
+}
+
+count_string() {
+    printf "%s" "$1" \
+        | sed "s/$2/##FILTER##\n/g" \
+        | grep -c "##FILTER##"
 }
 
 case "$1" in
@@ -51,13 +60,26 @@ case "$1" in
         ! "$path"helper/polybar_net_check.sh "pi" \
             && exit 1
 
-        data=$(request)
-        unreaded=$(extract_data "$data" 'reading-list",')
+        unreaded=$(request "unread-count")
+        unreaded=$(get_count "$unreaded" 'reading-list",')
+        starred=$(request "stream/contents/user/-/state/com.google/starred")
+        starred=$(count_string "$starred" '{"id":"tag')
 
         icon_rss="%{T2}參 %{T-}"
+        icon_star="%{T2}留 %{T-}"
 
-        [ "$unreaded" -gt 0 ] \
-            && "$path"helper/polybar_output.sh \
-                "$icon_rss$unreaded"
+        if [ "$unreaded" -gt 0 ] \
+            && [ "$starred" -gt 0 ]; then \
+                "$path"helper/polybar_output.sh \
+                    "$icon_rss$unreaded $icon_star$starred"
+        else
+            [ "$unreaded" -gt 0 ] \
+                && "$path"helper/polybar_output.sh \
+                    "$icon_rss$unreaded"
+
+            [ "$starred" -gt 0 ] \
+                && "$path"helper/polybar_output.sh \
+                    "$icon_star$starred"
+        fi
         ;;
 esac
