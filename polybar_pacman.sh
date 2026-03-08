08800432f8d2c2c0719e3468fd5b16021d67f3b5
mrdotx <klassiker@gmx.de>
2026-03-08 05:19:29 +0100

    added options for pacman/aur only

--- polybar_pacman.sh
+++ polybar_pacman.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/polybar/polybar_pacman.sh
 # author: klassiker [mrdotx]
 # url:    https://github.com/mrdotx/polybar
-# date:   2025-12-04T06:15:44+0100
+# date:   2026-03-08T05:19:20+0100
 
 # use standard c to identify paru ignored updates
 LC_ALL=C
@@ -12,10 +12,34 @@ LANG=C
 # source polybar helper
 . _polybar_helper.sh
 
-get_pacman_mirror() {
-    grep '^Server' /etc/pacman.d/mirrorlist \
+get_pacman_updates() {
+    pacman_mirror=$(grep '^Server' /etc/pacman.d/mirrorlist \
         | head -n1 \
         | cut -d'/' -f3
+    )
+
+    polybar_net_check "$pacman_mirror" \
+        && updates_pacman=$(checkupdates --nocolor 2> /dev/null | wc -l)
+}
+
+get_aur_updates() {
+    polybar_net_check "aur.archlinux.org" \
+        && updates_aur=$(paru -Qua 2> /dev/null | grep -c -v "\[ignored\]")
+}
+
+output_data() {
+    icon_pacman="%{T2}󰏗 %{T-}"
+    icon_aur="%{T2}󰏖 %{T-}"
+
+    if [ "${updates_pacman:-0}" -gt 0 ] && [ "${updates_aur:-0}" -gt 0 ]; then \
+        polybar_output "$icon_pacman$updates_pacman $icon_aur$updates_aur"
+    else
+        [ "${updates_pacman:-0}" -gt 0 ] \
+            && polybar_output "$icon_pacman$updates_pacman"
+
+        [ "${updates_aur:-0}" -gt 0 ] \
+            && polybar_output "$icon_aur$updates_aur"
+    fi
 }
 
 case "$1" in
@@ -25,28 +49,17 @@ case "$1" in
                 action "#packages.hook.0" >/dev/null 2>&1 &
         done
         ;;
+    --pacman)
+        get_pacman_updates
+        output_data
+        ;;
+    --aur)
+        get_aur_updates
+        output_data
+        ;;
     *)
-        polybar_net_check "$(get_pacman_mirror)" \
-            || exit 1
-
-        updates_pacman=$(checkupdates --nocolor 2> /dev/null | wc -l)
-        updates_aur=$(paru -Qua 2> /dev/null | grep -c -v "\[ignored\]")
-
-        icon_pacman="%{T2}󰏗 %{T-}"
-        icon_aur="%{T2}󰏖 %{T-}"
-
-        if [ "$updates_pacman" -gt 0 ] \
-            && [ "$updates_aur" -gt 0 ]; then \
-                polybar_output \
-                    "$icon_pacman$updates_pacman $icon_aur$updates_aur"
-        else
-            [ "$updates_pacman" -gt 0 ] \
-                && polybar_output \
-                    "$icon_pacman$updates_pacman"
-
-            [ "$updates_aur" -gt 0 ] \
-                && polybar_output \
-                    "$icon_aur$updates_aur"
-        fi
+        get_pacman_updates
+        get_aur_updates
+        output_data
         ;;
 esac
